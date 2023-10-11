d802629c541c8f640366024361aee3ea38dfeed2
StevenSCChang(張舜喬) <StevenSCChang@mobiledrivetech.com>
2023-10-11 09:06:25 +0800

    google analytics

--- docs/gitbook/gitbook-plugin-google-ads/plugin.js
+++ docs/gitbook/gitbook-plugin-google-ads/plugin.js
@@ -10,5 +10,17 @@ require(['gitbook'], function(gitbook) {
         adScript.setAttribute('crosssorigin', "anonymous");
         adScript.setAttribute('data-ad-client', firstClient);
         document.head.appendChild(adScript);
+
+        // add google analytic tag
+        var gaScript1 = document.createElement('script');
+        gaScript1.src = "https://www.googletagmanager.com/gtag/js?id=G-3GJMC6HG52";
+        document.head.appendChild(gaScript1);
+
+        var gaScript2 = document.createElement('script');
+        let code = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-3GJMC6HG52');`;
+        gaScript2.innerText = code;
+        document.head.appendChild(gaScript2);
+
+
     });
 });
---
+++ docs/pages/Ch09/Ch9.md
@@ -0,0 +1,3 @@
+# The IO Library
+
+[Ch9-1](Ch9-1.md)
